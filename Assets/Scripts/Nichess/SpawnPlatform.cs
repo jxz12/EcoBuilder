51ccd68322f9aa7ad86fa5e790ef2bc86196bfe7
Jonathan Zheng <jz@jonatharbookpro.lan>
2018-08-18 00:46:19 +0100

    fix buggy commit

---
+++ Assets/Scripts/Nichess/SpawnPlatform.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlatform : MonoBehaviour
+{
+	public void Spawn(Piece toSpawn)
+    {
+        // make things shiny
+        //toSpawn.transform.SetParent(transform, false);
+        Vector3 oldScale = toSpawn.transform.localScale;
+        Quaternion oldRotation = toSpawn.transform.localRotation;
+        toSpawn.transform.parent = transform;
+        toSpawn.transform.localPosition = Vector3.zero;
+        toSpawn.transform.localScale = oldScale;
+        toSpawn.transform.localRotation = oldRotation;
+
+        // make it glow and shit
+    }
+}
