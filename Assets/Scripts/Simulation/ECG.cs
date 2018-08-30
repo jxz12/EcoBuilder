54f68593ebf6fc8d5db40254dba42a3dff916239
jxz12 <jxz12@ic.ac.uk>
2018-08-30 18:32:27 +0100

    added trees and cowboy hat. Initial steps for ECG

--- Assets/Scripts/Simulation/ECG.cs
+++ Assets/Scripts/Simulation/ECG.cs
@@ -1,9 +1,26 @@
 using UnityEngine;
+using System;
+using System.Collections.Generic;
 
 public class ECG : MonoBehaviour
 {
-	public void Bleep()
+	[SerializeField] GameObject linePrefab;
+
+	Func<float, float> yAxisScale = x=>Mathf.Log10(x);
+	Dictionary<int, ECGLine> lines = new Dictionary<int, ECGLine>();
+	private Transform worldParent, worldGrandParent;
+	
+	void Awake()
+	{
+	}
+
+	public void AddLine(int idx)
 	{
 
+	}
+	// TODO: make this bleeping change transparency
+	public void Bleep(int idx, float value)
+	{
+	
 	}
 }
