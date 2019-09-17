c9f16eed33d0f2f9500d472ca705f032147bfff4
Archie <archie@dyn3208-55.wlan.ic.ac.uk>
2019-09-17 13:28:35 +0100

    A basic version of quickhull in python (still needs some work) and a rough plan for the structure of BMesh

---
+++ Assets/Archie/Scripts/BMesh.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using System.Diagnostics; // TIME TESTING
+
+namespace EcoBuilder.Archie
+{
+
+
+    public class Bmesh : MonoBehaviour
+    {
+        public class skeletal_joint
+        {
+            public Vector3 position;
+            public float size;
+            public List<skeletal_joint> linked;
+            public static void Link(skeletal_joint a, skeletal_joint b)
+            {
+                a.linked.Add(b);
+                b.linked.Add(a);
+            }
+            public skeletal_joint(Vector3 position_of_joint = new Vector3(), float size_of_joint = 1)
+            {
+                position = position_of_joint;
+                size = size_of_joint;
+                linked = new List<skeletal_joint>();
+            }
+
+            public static skeletal_joint String_to_Skeleton(string system)
+            {
+                // turns an l-system string into a skeleton
+            }
+        }
+    }
+}
