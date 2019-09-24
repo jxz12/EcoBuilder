170d3eb9c3390d682dd17dc20875798da6e96aa3
Archie <archie@dyn3208-81.wlan.ic.ac.uk>
2019-09-24 12:43:02 +0100

    midway through fixing QuickHull3D.cs - It turned out that QuickHull3D algorithm was much more complicated than QuickHull2D (and than I expected)

---
+++ Assets/Archie/Scripts/AnimalTexture.cs
@@ -0,0 +1,13 @@
+// // animal texture maker
+
+// public class AnimalTexture
+// {
+
+//     public static Generate(int seed, face_uv_coordiantes)
+//     {
+//         // seed random number
+//         // create new texture array
+//         for
+
+//     }
+// }
