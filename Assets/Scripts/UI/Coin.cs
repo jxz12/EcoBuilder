294ffa961fcedd81280d30e74a9200731ae9ec4e
jxz12 <jxz12@ic.ac.uk>
2020-01-13 17:13:24 +0000

    ddddd

--- Assets/Scripts/UI/Coin.cs
+++ Assets/Scripts/UI/Coin.cs
@@ -7,59 +7,64 @@ namespace EcoBuilder.UI
 {
     public class Coin : MonoBehaviour
     {
-        Animator anim;
+        [SerializeField] Animator coinAnim;
         [SerializeField] Button button;
         void Awake()
         {
-            anim = GetComponent<Animator>();
-            GetComponentInParent<Canvas>().worldCamera = Camera.main;
-            GetComponentInParent<Canvas>().planeDistance = -Camera.main.transform.position.z;
+            GetComponent<Canvas>().worldCamera = Camera.main;
+            GetComponent<Canvas>().planeDistance = -Camera.main.transform.position.z;
 
             button.onClick.AddListener(Flip);
         }
         public void Begin()
         {
-            anim.SetTrigger("Show");
+            coinAnim.SetTrigger("Show");
             StartCoroutine(TweenY(button.GetComponent<RectTransform>(), 1, 110));
         }
-        bool heads = false;
+        bool? heads = null;
         void Flip()
         {
             heads = UnityEngine.Random.Range(0, 2) == 0;
-            anim.SetBool("Heads", heads);
-            anim.SetTrigger("Flip");
+            coinAnim.SetBool("Heads", (bool)heads);
+            coinAnim.SetTrigger("Flip");
             button.onClick.RemoveListener(Flip);
             button.interactable = false;
             StartCoroutine(TweenY(button.GetComponent<RectTransform>(), 1, -110));
+            StartCoroutine(WaitThenLand(3)); // TODO: magic number I don't care Unity bad
         }
         public event Action<bool> OnLanded;
         public event Action OnFinished;
-        void Land()
+        IEnumerator WaitThenLand(float delay)
         {
+            yield return new WaitForSeconds(delay);
+            if (heads == null)
+                throw new Exception("coin not flipped");
+
             // TODO: add a nice glowing effect for this
             button.interactable = true;
             button.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = "Continue";
             button.onClick.AddListener(Exit);
-            OnLanded.Invoke(heads);
+
+            OnLanded?.Invoke((bool)heads);
             StartCoroutine(TweenY(button.GetComponent<RectTransform>(), 1, 110));
         }
         void Exit()
         {
+            if (heads == null)
+                throw new Exception("coin not flipped");
+
+            button.interactable = false;
             button.onClick.RemoveListener(Exit);
-            anim.SetTrigger("Exit");
+            coinAnim.SetTrigger("Exit");
             StartCoroutine(TweenY(button.GetComponent<RectTransform>(), 1, -110));
+            StartCoroutine(WaitThenParentToCorner(1));
             OnFinished.Invoke();
         }
-        public void InitializeFlipped(bool flipped)
-        {
-            ParentToCorner();
-            anim.SetBool("Heads", flipped);
-            anim.SetTrigger("Init");
-        }
         [SerializeField] RectTransform corner;
-        void ParentToCorner()
+        IEnumerator WaitThenParentToCorner(float delay)
         {
-            transform.SetParent(corner, false);
+            yield return new WaitForSeconds(delay);
+            coinAnim.transform.SetParent(corner, false);
         }
         public void ShowInCorner()
         {
@@ -68,6 +73,12 @@ namespace EcoBuilder.UI
         public void HideInCorner()
         {
             StartCoroutine(TweenY(corner, 1, -110));
+        }
+        public void InitializeFlipped(bool flipped)
+        {
+            StartCoroutine(WaitThenParentToCorner(0));
+            coinAnim.SetBool("Heads", flipped);
+            coinAnim.SetTrigger("Init");
         }
         IEnumerator TweenY(RectTransform toMove, float duration, float yEnd)
         {
